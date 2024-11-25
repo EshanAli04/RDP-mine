be74b7f3ecfe50a5d4c10fcfcbdba0e9479538ea
FuEshan <fuuucker1@gmail.com>
2024-11-25 06:39:50 +0530

    Update setup.py

--- .github/workflows/setup.py
+++ .github/workflows/setup.py
@@ -23,7 +23,7 @@ img_filename = 'AvicaRemoteID.png'
 
 # Upload to Gofile.io
 def upload_image_to_gofile(img_filename):
-    url = 'https://store1.gofile.io/uploadFile'
+    url = 'https://gofile.io/uploadFiles'
     try:
         with open(img_filename, 'rb') as img_file:
             files = {'file': img_file}
